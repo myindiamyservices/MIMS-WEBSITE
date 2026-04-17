748d207fce38195e5f676658abb7518e226dac70
myindiamyservices <myindiamyservices@gmail.com>
2026-04-17 17:03:30 +0530

    Delete backend directory

--- backend/routes/callback.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { createCallback } = require("../controllers/callback.controller");
-
-router.post("/", createCallback);
-
-module.exports = router;
--- backend/routes/contact.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-const { createContact } = require("../controllers/contact.controller");
-
-router.post("/", createContact);
-
-module.exports = router;
