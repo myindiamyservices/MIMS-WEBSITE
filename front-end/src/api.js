82796e3db57c75389083624a491ee2695c41669a
Sarthak <myindiamyservices@gmail.com>
2026-04-18 18:37:55 +0530

    Support VITE_API_BASE_URL for Netlify

--- front-end/src/api.js
+++ front-end/src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || "https://mims-backend.onrender.com",
+    import.meta.env.VITE_API_BASE_URL ||
+    import.meta.env.VITE_API_URL ||
+    "https://mims-backend.onrender.com",
 });
 
 export default API;
